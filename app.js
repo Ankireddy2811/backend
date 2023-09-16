7c7e5b7782a54a33446810e5aed6173cb0051563
Ankireddy99 <20kb1a05d2@nbkrist.org>
2023-09-16 22:29:20 +0530

    add backend

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
-const bcrypt = require("bcrypt");
 
 
 const path = require("path");
 const {open} = require("sqlite");
 const sqlite3 = require("sqlite3");
-const { request } = require("http");
+// const { request } = require("http");
 
 let db=null;
-let dbPath = path.join(__dirname,"reg.db");
+let dbPath = path.join(__dirname,"users.db");
 const initalizeDbAndServer = async()=>{
 try{
 db = await open({
@@ -20,7 +19,7 @@ db = await open({
     driver:sqlite3.Database
 })
 app.listen(3011,()=>{
-    console.log("Server is running at https://localhost:3011 ")
+    console.log("Server is running at https://localhost:3011/")
 })
 }
 catch (e){
@@ -30,35 +29,35 @@ catch (e){
 
 }
 
-initalizeDbAndServer();
+initalizeDbAndServer()
 
 app.get("/",async(request,response)=>{
-  const myQuery = `SELECT * FROM firsttable;`;
+  const myQuery = `SELECT * FROM usersInfo;`;
   const myResponse = await db.all(myQuery)
   response.send(myResponse)
 })
 
 app.post("/users",async(request,response)=>{
-    const {name,username,password,gender,phoneno} = request.body;
-    const personQuery = `SELECT * FROM firsttable WHERE username = '${username}';`
-    const personResults = await db.get(personQuery);
-    
-    if (personResults === undefined){
-        const hashedPassword = await bcrypt.hash(password,10)
-        const addQuery = `INSERT INTO firsttable (name,username,password,gender,phoneno) VALUES ('${name}','${username}','${hashedPassword}','${gender}',${phoneno});`;
-        const addResponse = await db.run(addQuery)
-        response.send("SUCCESSFULLY REGISTERED");
-      
-    }
-    else{
-      
-      response.send("Already user exits")
-      response.status(400);
-      
-    }
-   
-})
+    const {name,email,imageurl} = request.body;
+    const addQuery = `INSERT INTO usersInfo (name,email,imageurl) VALUES ('${name}','${email}','${imageurl}');`;
+    const addRespose = await db.run(addQuery)
+    response.send("user details added successfully")
+}) 
 
+app.put("/users/:id",async(request,response)=>{
+    const {id} = request.params
+    const {name,email,imageurl} = request.body;
+    const updateQuery = `UPDATE usersInfo SET name = '${name}',email= '${email}',imageurl='${imageurl}' WHERE id=${id};`;
+    const updateRespose = await db.run(updateQuery )
+    response.send("user details updated successfully")
+}) 
+
+app.delete("/users/:id",async(request,response)=>{
+    const {id} = request.params
+    const deleteQuery = `DELETE FROM usersInfo WHERE id=${id}`
+    await db.run(deleteQuery)
+    response.send("user details deleted successfuuly")
+})
 
 
   
